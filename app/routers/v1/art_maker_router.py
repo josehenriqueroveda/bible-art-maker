931a7766005a682f63f9e5b71b4f583fd0843077
josehenriqueroveda <zeh.henrique92@gmail.com>
2023-07-25 16:44:19 -0300

    Generate temp images

--- app/routers/v1/art_maker_router.py
+++ app/routers/v1/art_maker_router.py
@@ -1,8 +1,9 @@
+import os
 import logging
 import textwrap
 
-from fastapi import APIRouter, Request
-from fastapi.responses import JSONResponse
+from fastapi import APIRouter, Request, BackgroundTasks
+from fastapi.responses import FileResponse, JSONResponse
 from PIL import Image, ImageDraw, ImageFont
 
 from models.VerseRequest import VerseRequest
@@ -21,7 +22,7 @@ logger = logging.getLogger(__name__)
 art_maker_router = APIRouter()
 
 
-def generate_verse_images(verse_request: VerseRequest) -> None:
+def generate_verse_images(verse_request: VerseRequest) -> str | None:
     bible = Bible(f"{BOOKS_DIR}{verse_request.version}.json")
     verse_text = bible.get_text(verse_request)
     if verse_text is None:
@@ -30,21 +31,23 @@ def generate_verse_images(verse_request: VerseRequest) -> None:
 
     if verse_request.end_verse is None:
         verse_info = f"{verse_request.book} {verse_request.chapter}:{verse_request.start_verse} ({verse_request.version.upper()})"
-        image_path = f"images/{verse_request.book}_{verse_request.chapter}_{verse_request.start_verse}.jpg"
+        image_path = f"temp/{verse_request.book}_{verse_request.chapter}_{verse_request.start_verse}.jpg"
     else:
         verse_info = f"{verse_request.book} {verse_request.chapter}:{verse_request.start_verse}-{verse_request.end_verse} ({verse_request.version.upper()})"
-        image_path = f"images/{verse_request.book}_{verse_request.chapter}_{verse_request.start_verse}-{verse_request.end_verse}.jpg"
+        image_path = f"temp/{verse_request.book}_{verse_request.chapter}_{verse_request.start_verse}-{verse_request.end_verse}.jpg"
 
     if verse_request.mobile:
         image = Image.open(BACKGROUND_IMAGE_MOBILE)
         wrapper_width = 30
         font_size_verse = 28
         font_size_info = 22
+        border_width = 2
     else:
         image = Image.open(BACKGROUND_IMAGE)
         wrapper_width = 50
         font_size_verse = 40
         font_size_info = 32
+        border_width = 3
 
     font_text = ImageFont.truetype("arialbd.ttf", font_size_verse)
     font_info = ImageFont.truetype("arial.ttf", font_size_info)
@@ -56,7 +59,6 @@ def generate_verse_images(verse_request: VerseRequest) -> None:
     verse_text = "\n".join(lines)
 
     border_color = (0, 0, 0)
-    border_width = 3
 
     _, _, w1, h1 = draw.multiline_textbbox(
         (0, 0), verse_text, font=font_text, spacing=LINE_SPACING
@@ -84,24 +86,40 @@ def generate_verse_images(verse_request: VerseRequest) -> None:
     )
 
     image.save(image_path)
-
     logger.info("Image generation completed.")
+
+    return image_path
 
 
 @art_maker_router.post("/image/verse", tags=["Image"])
 @LIMITER.limit("1/min")
-async def art_maker(verse_request: VerseRequest, request: Request):
+async def art_maker(
+    verse_request: VerseRequest, request: Request, background_tasks: BackgroundTasks
+):
     """
-    Generates an image with the requested Bible verse and returns a JSON response with a message indicating
-    that the image generation has been completed.
+    Generates an image with the requested Bible verse.
 
     Args:
         verse_request (VerseRequest): The request object containing the book, chapter, and verse range.
 
     Returns:
-        JSONResponse: A JSON response with a message indicating that the image generation has been completed.
+        FileResponse: The image file.
     """
-    generate_verse_images(verse_request)
-    return JSONResponse(
-        status_code=200, content={"message": "Image generation completed."}
+    image_path = generate_verse_images(verse_request)
+    if image_path is None:
+        return JSONResponse(
+            status_code=400,
+            content={"message": "Invalid book, chapter, or verse range."},
+        )
+
+    def delete_image():
+        try:
+            os.remove(image_path)
+        except OSError:
+            pass
+
+    background_tasks.add_task(delete_image)
+
+    return FileResponse(
+        image_path, media_type="image/jpeg", filename=os.path.basename(image_path)
     )
